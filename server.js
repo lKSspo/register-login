49ca54d771219ed8e534a6c5aa25224d8a620173
Alessandro0003 <alessandroquinta3@gmail.com>
2024-02-26 18:24:42 -0300

    ⚙  chore: settings application to redenrization with html

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require('express')
+var bodyParser = require('body-parser')
+const path = require('path')
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+app.set('views', path.join(__dirname, '/web'))
+
+app.get('/', (req, res) => {
+    res.render('home', {})
+})
 
 const port = 8080
 app.listen(port, () => {
